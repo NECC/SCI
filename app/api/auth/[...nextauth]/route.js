069bffce55cc13285884faf47f43e4d88e5f3230
pedroacamargo <pedroaugennes@gmail.com>
2023-11-27 00:05:11 +0000

    nextauth start

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,73 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials) {
+        try {
+          const findUser = await prisma.user.findUnique({
+            where: {
+              email: credentials.email,
+            },
+          });
+
+          if (findUser) {
+            console.log("User Exists!");
+
+            const match = await bcrypt.compare(
+              credentials.password,
+              findUser.password
+            );
+
+            if (match) {
+              console.log("Passwords Match!");
+              delete findUser.password;
+              return findUser;
+            }
+          }
+        } catch (error) {
+          console.log(error);
+          return null;
+        }
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      session.user = token;
+      return session;
+    },
+  },
+  session: { strategy: "jwt" }
+};
+
+const handler = async (req, context) => {
+  console.log("THIS IS AN HANDLER\n", req.url);
+
+  if (req.method == "HEAD") {
+    return new Response(null, { status: 200 });
+  }
+
+  return await NextAuth(req, context, authOptions);
+};
+
+export { handler as GET, handler as POST, handler as HEAD };
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,28 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [user, setUser] = useState({});
+
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      return redirect("/api/auth/signin");
+    },
+  });
+
+  useEffect(() => {
+    if (session) setUser(session.user);
+  }, [session]);
+
+  console.log(user);
+
   return (
     <main className="">
-      
+      <h1 className="text-2xl">Welcome {user.name}</h1>
     </main>
-  )
+  );
 }
