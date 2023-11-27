35a8e256cbd34ce36f160fad91ab255a5be0c2fd
carlod035 <carlosdf035@gmail.com>
2023-11-27 15:55:46 +0000

    backup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
-/** @type {import('tailwindcss').Config} */
-export const content = [
-  './pages/**/*.{js,ts,jsx,tsx,mdx}',
-  './components/**/*.{js,ts,jsx,tsx,mdx}',
-  './app/**/*.{js,ts,jsx,tsx,mdx}',
-];
-export const theme = {
-  extend: {
-    colors: {
-      'custom-blue': '#4349f7', /* cor do logo das jornadas poderá ser utilizado */
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': {
+          1: '#0583D2',
+          2: '#B8E3FF',
+        }, // cor do logo das jornadas poderá ser utilizado
+      },
+      fontFamily: {
+        'comfortaa': ['Comfortaa', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+      },  
     },
   },
+  plugins: [],
 };
-export const plugins = [];
