153a1f4023075bf14b14f9b000f10696affb8b7e
carlod035 <carlosdf035@gmail.com>
2023-12-18 01:23:44 +0000

    hexa_finish

--- app/page.jsx
+++ app/page.jsx
@@ -37,8 +37,8 @@ export default function Home() {
   return (
     <main className="">
       <Nav />
-      <div className="bg-[url('/banner.png')] bg-no-repeat bg-cover h-[600px] w-full flex justify-center ">
-        <div className="w-4/5 flex items-center relative">
+      <div className="bg-[url('/banner.png')] bg-no-repeat bg-cover h-[600px] w-full flex justify-center relative ">
+        <div className="w-4/5 flex items-center ">
           <div className="w-6/12 flex flex-col gap-4">
             <h1 className="text-white text-6xl font-extrabold leading-tight">SEMANA DA CIÊNCIA E INOVAÇÃO</h1>
             <p className='text-white text-xl'>14 - 15 MARÇO</p>
@@ -49,13 +49,25 @@ export default function Home() {
                 EVENTOS
               </Link>
             </div>
-            <div className='absolute bottom-4 right-0 w-[500px] h-[500px] bg-black'>
+            <div className='absolute bottom-4 right-0 w-[500px] h-[350px]  '>
               <div className="relative">
-                <div className="p-5 bg-yellow-400 w-2 rounded-full absolute left-1/2 -translate-x-1/2 z-10"></div>
-                <div className="p-2 w-40 bg-white rounded-lg absolute -rotate-[40deg] translate-y-[70px] translate-x-[95px] z-10"></div>
-                <div className="p-2 w-40 bg-white rounded-lg absolute rotate-[40deg]  translate-y-[70px] -translate-x-[95px] right-0 z-10"></div>
-                <div className="p-5 bg-yellow-400 w-2 rounded-full absolute  translate-x-[85px] translate-y-[120px] z-0"></div>
-                <div className="p-5 bg-yellow-400 w-2 rounded-full absolute  -translate-x-[85px] translate-y-[120px] z-0 right-0"></div>
+                <div className="p-5 bg-amber-400 w-2 rounded-full absolute left-1/2 -translate-x-1/2 translate-y-[5px]  z-10"></div>
+                <div className="p-2 w-[130px] bg-white opacity-25 rounded-lg absolute -rotate-[30deg] translate-y-[55px] translate-x-[120px] z-0"></div>
+                <div className="p-2 w-[130px] bg-white opacity-25 rounded-lg absolute rotate-[30deg]  translate-y-[55px] -translate-x-[120px] right-0 z-0"></div>
+                <div className="p-5 bg-orange-600 opacity-95 w-2 rounded-full absolute translate-x-[100px] translate-y-[85px] z-10"></div>
+                <div className="p-5 bg-orange-600 opacity-95 w-2 rounded-full absolute -translate-x-[100px] translate-y-[85px] z-10 right-0"></div>
+                <div className="p-2 w-[130px] bg-white opacity-25 rounded-lg absolute -rotate-[90deg] translate-y-[175px] translate-x-[55px] z-0"></div>
+                <div className="p-2 w-[130px] bg-white opacity-25 rounded-lg absolute -rotate-[90deg] translate-y-[175px] -translate-x-[55px]  right-0 z-0"></div>
+                <div className="p-5 bg-amber-400 opacity-90 w-2 rounded-full absolute  translate-x-[100px] translate-y-[238px] z-10"></div>
+                <div className="p-5 bg-amber-400 opacity-90 w-2 rounded-full absolute -translate-x-[100px] translate-y-[238px] right-0 z-10 "></div>
+                <div className="p-2 w-[130px] bg-white opacity-25 rounded-lg absolute rotate-[30deg] translate-y-[285px] translate-x-[120px] z-0"></div>
+                <div className="p-2 w-[130px] bg-white opacity-25 rounded-lg absolute -rotate-[30deg] translate-y-[285px] -translate-x-[120px] right-0 z-0"></div>
+                <div className="p-5 bg-amber-400 w-2 rounded-full absolute left-1/2 -translate-x-1/2 translate-y-[305px]  z-10"></div>
+                <div className="p-2 w-[280px] bg-white opacity-25 rounded-lg absolute -rotate-[30deg] translate-y-[170px] translate-x-[110px]  z-0"></div>
+                <div className="p-2 w-[280px] bg-white opacity-25 rounded-lg absolute rotate-[30deg] translate-y-[170px] translate-x-[110px]  z-0"></div>
+                <div className="p-2 w-[900px] bg-white rounded-lg absolute -rotate-[90deg] translate-y-[450px] left-1/2 -translate-x-1/2 z-0"></div>
+                <div className="p-5 bg-amber-400 w-2 rounded-full absolute left-1/2 -translate-x-1/2 translate-y-[160px]  z-10"></div>
+                <div className="p-5 bg-black border-white border-2 w-2 rounded-full absolute left-1/2 -translate-x-1/2 translate-y-[550px]  z-10"></div>
               </div>
 
             </div>
@@ -64,6 +76,9 @@ export default function Home() {
 
 
         </div>
+      </div>
+      <div className="w-full h-screen bg-black">
+
       </div>
     </main>
   );
