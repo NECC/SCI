c4611605ac0e03f81e03f4a6e3a236253e56d625
luissilva <biokiller134@gmail.com>
2024-02-05 11:08:32 +0000

    Page overhaul

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -8,22 +8,33 @@ import {
     CardHeader,
     CardBody,
     CardFooter,
-    Image,
     Button,
-    Chip,
     Spinner,
+    ButtonGroup,
   } from "@nextui-org/react";
-  import axios from "axios";
-  import { useRouter } from "next/navigation";
-
-  
-  
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { 
+    SlArrowLeft,
+    SlArrowRight 
+} from "react-icons/sl";
+import { IconContext } from "react-icons";
+import { FaRegCalendarAlt } from "react-icons/fa";
 
 export default function Profile () {
     const [user, setUser] = useState({});
-    const [enrollment, setEnrollment] = useState([]);
-    const [count, setCount] = useState(0);
-    const [place,setPlace] = useState(1);
+    const [userEnrollment, setUserEnrollment] = useState([]);
+    const [activities, setActivities] = useState ([]);
+    const [place,setPlace] = useState(0);
+    const [firstPlace,setFirstPlace] = useState(0);
+    const [type,setType] = useState(null);
+    const [workshops,setWorkshops] = useState(0);
+    const [talks,setTalks] = useState(0);
+    const [others,setOthers] = useState(0);
+    const [activeScreen,setActiveScreen] = useState(0);
+    const [activeDay,setActiveDay] = useState(18);
+    let count = 1;
+    let maxCount = 0;
     const router = useRouter();
 
     const { data: session, status} = useSession({
@@ -38,7 +49,7 @@ export default function Profile () {
     }, [session]);
 
     function filterByUser(e) {
-        if (e.userId == user.id) {
+        if (e.userId == user.id) { 
             return true;
         }
         return false;
@@ -48,35 +59,112 @@ export default function Profile () {
         // gets enrollments and filters them by the current user
         const { data } = await axios.get("/api/enrollments");
         const fdata = data.enrollments.filter(filterByUser);
-        setEnrollment(fdata);
+        setUserEnrollment(fdata);
+        setType("WORKSHOP");
     }
 
-    
-    
-    if(count == 0) getUserEnrollments() && setCount(1);
+    function filterByType(e) {
+        if(e.activity.type == type) {
+            return true;
+        }
+        return false;
+    }
+
+    useEffect (() => {
+        // sets which activities to show to the user
+        setActivities(userEnrollment.filter(filterByType));   
+    },[type])
+
+    function getNumberOfActivity(a,t) {
+        let count = 0;
+        a.forEach((v) => (v.activity.type == t && count++));
+        return count;
+    }
+
+    useEffect (() => {
+        // get amount of each activity that the user is enrolled in 
+        setWorkshops(getNumberOfActivity(userEnrollment,"WORKSHOP"));
+        setTalks(getNumberOfActivity(userEnrollment,"TALK"));
+        setOthers(getNumberOfActivity(userEnrollment,"OTHER")); 
+    },[userEnrollment])
+
+    useEffect (() => {
+        getUserEnrollments();
+    },[])
+
+    const getUserPlace = async () => {
+        // compares current user points with the rest of the users to determine current user's place and how many points first place has
+        const {data} = await axios.get("/api/users");
+        for (let i = 0; i < data.users.length; i++) {
+            if (data.users[i].points > user.points && data.users[i].id != user.id){ 
+                count++;
+            }
+            if (data.users[i].points > maxCount) {maxCount = data.users[i].points};
+        }
+        setPlace(count);
+        setFirstPlace(maxCount);
+    }
+
+    useEffect (() => {
+        getUserPlace();
+    },[user])
+
+    function previousDay() {
+        if (activeDay > 18) setActiveDay(activeDay - 1);
+        else setActiveDay(18);
+    }
+
+    function nextDay() {
+        if (activeDay < 21) setActiveDay(activeDay + 1);
+        else setActiveDay(21);
+    }
+
+    console.log(activities);
 
     return (
-        <div className="bg-black h-screen bg-[url('/rectangle.png')] bg-no-repeat bg-top bg-cover">
+        <div className="bg-white dark:bg-black h-screen bg-[url('/rectangle.png')] bg-no-repeat bg-top bg-cover overflow-y-scroll no-scrollbar">
             <Nav />
             <div className="gap-2 px-8 pt-20">
-                {(status != "loading") ? (
-                <Card isBlurred className="w-full h-[700px]">
-                    <CardHeader className="bg-black/40 border-b-1 border-default-600 dark:border-default-100">
-                        <div className="flex flex-grow gap-2 items-center p-2">
-                            <div className="flex flex-col text-white/90 uppercase font-black text-2xl">
+                {(status != "loading" && activeScreen == 0) ? (
+                <Card isBlurred className="w-full h-[625px]">
+                    <CardHeader className="dark:bg-black/40 border-b-1 border-default-600 dark:border-default-100 flex-row">
+                        <div className="flex flex-grow gap-2 items-center p-2 justify-between">
+                            <div className="flex flex-col text-neutral-700 dark:text-white/90 uppercase font-black text-2xl">
                                 Bem-vindo {user.name}
                             </div>
+                            <ButtonGroup>
+                                <Button onClick={() => setActiveScreen(0)} className="bg-[#023f65] text-white dark:bg-slate-300 dark:text-black border-1"> Atividades Inscritas </Button>
+                                <Button onClick={() => setActiveScreen(1)} className="dark:text-white dark:bg-transparent dark:border-gray-50 border-1"> Ranking </Button>
+                            </ButtonGroup>
                         </div>
                     </CardHeader>
-                    <CardBody className="bg-black/40 flex flex-col gap-10">
-                        <div className="gap-2 flex flex-col">
-                            <h className="px-1 text-white font-semibold text-l"> Atividades Inscritas </h>
-                            <div className="flex gap-4 flex-col md:flex-row">
-                                {
-                                    enrollment.map((enrollments, index) => (
+                    <CardBody className="dark:bg-black/40 flex flex-col">
+                        <div className="flex flex-col gap-y-1">
+                            <h className="mx-1 text-[#494748] dark:text-white font-semibold text-2xl"> Atividades Inscritas </h>
+                            <div className="mx-5 flex flex-row items-center">
+                                <h2 className="font-bold text-lg"> Dia {activeDay} </h2>
+                                <Button className="bg-transparent flex min-w-3" size='sm' onClick={() => previousDay()}> <SlArrowLeft size={'1em'}/> </Button>
+                                <FaRegCalendarAlt size={'1em'} />
+                                <Button className="bg-transparent flex min-w-3" size='sm' onClick={() => nextDay()}> <SlArrowRight size={'1em'}/> </Button> 
+                            </div>
+                            <div className="mx-6 mt-1 -mb-3 flex flex-row h-8">
+                                <ButtonGroup>
+                                    <Button className={(type != "WORKSHOP") ? "text-white bg-neutral-500 dark:bg-transparent dark:border-gray-50 border-1" : "bg-[#023f65] text-white dark:bg-slate-300 dark:text-black border-1"} onClick={() => setType("WORKSHOP")}>
+                                        Workshops : {workshops}
+                                    </Button>
+                                    <Button className={(type != "TALK") ? "text-white bg-neutral-500 dark:bg-transparent dark:border-gray-50 border-1" : "bg-[#023f65] text-white dark:bg-slate-300 dark:text-black border-1"} onClick={() => setType("TALK")}>
+                                        Talks : {talks}
+                                    </Button>
+                                    <Button className={(type != "OTHER") ? "text-white bg-neutral-500 dark:bg-transparent dark:border-gray-50 border-1" : "bg-[#023f65] text-white dark:bg-slate-300 dark:text-black border-1"} onClick={() => setType("OTHER")}>
+                                        Others : {others}
+                                    </Button>
+                                </ButtonGroup>
+                            </div>
+                            <div className="no-scrollbar mx-6 flex flex-wrap gap-4 flex-col md:flex-row w-[300px] md:w-auto mt-8">
+                                { (activities.length > 0) ? (
+                                    activities.map((enrollments, index) => (
                                         <div className="flex" key={index}>
-                                            { (enrollments.user.id == user.id) ? (
-                                                <Card className="w-[300px]">
+                                                <Card className="w-[300px] border-1 border-[#222327]">
                                                     <CardBody>
                                                         <h4 className="dark:text-white/90 font-bold text-xl">
                                                             {enrollments.activity.title}
@@ -102,24 +190,41 @@ export default function Profile () {
                                                         </div>
                                                     </CardFooter>
                                                 </Card>
-                                            ) : (<></>)
-                                            }
                                         </div>
                                     ))
+                                ) : <p className="h-[117px]"> Não estás inscrito em nenhuma atividade deste tipo </p>
                                 }
                             </div>   
                         </div>
-                        <div>
-                            <h className="px-1 text-white font-semibold text-l"> Ranking </h>
-                            <div>
-                                <p> Atualmente tens {user.points} pontos </p> 
-                                <p> Estás atualmente em {place}º lugar </p>
-                            </div>
-                        </div>
                     </CardBody>    
                 </Card>
-                ) : (<Spinner color="white" size="lg"/>)
-                }
+                ) : ((activeScreen == 1) ? (
+                    <Card className="h-[625px] w-full">
+                        <CardHeader className="dark:bg-black/40 border-b-1 border-default-600 dark:border-default-100 flex-row">
+                            <div className="flex flex-grow gap-2 items-center p-2 justify-between">
+                                <div className="flex flex-col dark:text-white/90 uppercase font-black text-2xl">
+                                    Bem-vindo {user.name}
+                                </div>
+                                <div>
+                                    <ButtonGroup>
+                                        <Button onClick={() => setActiveScreen(0)} className="dark:text-white dark:bg-transparent dark:border-gray-50 border-1"> Atividades Inscritas </Button>
+                                        <Button onClick={() => setActiveScreen(1)} className="bg-[#023f65] text-white dark:bg-slate-300 dark:text-black border-1"> Ranking </Button>
+                                    </ButtonGroup>
+                                </div>
+                            </div>
+                        </CardHeader>
+                        <CardBody className="dark:bg-black/40 flex flex-col ">
+                            <div className="px-1"> 
+                                <h className="dark:text-white font-semibold text-l"> Ranking </h>
+                                <div>
+                                <p> Tens {user.points} pontos </p> 
+                                <p> Estás em {place}º lugar </p>
+                                {(place > 1) ? (<p> O primeiro lugar têm {firstPlace} pontos</p>) : (<></>)}
+                                </div>
+                            </div>
+                        </CardBody>
+                    </Card>
+                    ) : (<Spinner color="white" size="lg"/>))}
             </div>
         </div>
     );
