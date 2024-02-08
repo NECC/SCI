d96b68f5c2ee92948ce87234c461d48a5acac2e4
carlod035 <carlosdf035@gmail.com>
2024-02-08 00:56:00 +0000

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,20 +2,22 @@ import Image from 'next/image'
 import { IoLogoGithub } from "react-icons/io";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import Linker from 'next/link'
 const Footer = () => {
   return (
-    <div className='flex justify-between w-11/12 items-center m-auto '>
-      <div className="flex gap-4">
-        <IoLogoGithub className="hover:opacity-60 cursor-pointer" size={25} />
-        <FaInstagram className="hover:opacity-60 cursor-pointer" size={25} />
-        <FaLinkedin className="hover:opacity-60 cursor-pointer" size={25} />
+    <div className='w-full'>
+      <div className='flex md:flex-row flex-col justify-between items-center gap-4 p-4 w-11/12 m-auto z-50'>
+        <div className="flex gap-4">
+          <IoLogoGithub className="hover:opacity-60 cursor-pointer" size={25} color={"white"} />
+          <FaInstagram className="hover:opacity-60 cursor-pointer" size={25} color={"white"} />
+          <FaLinkedin className="hover:opacity-60 cursor-pointer" size={25} color={"white"} />
+        </div>
+        <Linker href="/#footer" className='order-first md:order-none'><Image src="/sci-logo.png " alt="logo" width={70} height={70} className='cursor-pointer' /> </Linker>
+        <a className='hover:underline decoration-1 cursor-pointer text-white'>General Information</a>
       </div>
-      <Image src="/sci-logo.png" alt="logo" width={70} height={70} />
-      <a>General</a>
-
-
     </div>
   )
 }
 
 export default Footer
+
