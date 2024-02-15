a452a51f195f681591087042f550792e1e7da2e0
carlod035 <carlosdf035@gmail.com>
2024-02-15 20:47:30 +0000

    color

--- components/Hexa.jsx
+++ components/Hexa.jsx
@@ -7,19 +7,19 @@ const Hexa = () => {
       <div className="p-4 bg-blue-950 w-2 rounded-full absolute left-1/2 -translate-x-1/2 translate-y-[0px] z-50"></div>
       <div className="p-1 w-[130px] bg-white opacity-25 rounded-lg absolute -rotate-[30deg] translate-y-[55px] translate-x-[120px]"></div>
       <div className="p-1 w-[130px] bg-white opacity-25 rounded-lg absolute rotate-[30deg]  translate-y-[55px] -translate-x-[120px] right-0"></div>
-      <div className="p-4 bg-amber-400 w-2 rounded-full absolute translate-x-[100px] translate-y-[85px] z-50"></div>
-      <div className="p-4 bg-amber-400 w-2 rounded-full absolute -translate-x-[100px] translate-y-[85px] z-50 right-0"></div>
+      <div className="p-4 bg-yellow-300 w-2 rounded-full absolute translate-x-[100px] translate-y-[85px] z-50"></div>
+      <div className="p-4 bg-yellow-300 w-2 rounded-full absolute -translate-x-[100px] translate-y-[85px] z-50 right-0"></div>
       <div className="p-1 w-[130px] bg-white rounded-lg absolute -rotate-[90deg] translate-y-[175px] translate-x-[55px]"></div>
       <div className="p-1 w-[130px] bg-white rounded-lg absolute -rotate-[90deg] translate-y-[175px] -translate-x-[55px] right-0"></div>
-      <div className="p-4 bg-amber-400 opacity-90 w-2 rounded-full absolute  translate-x-[100px] translate-y-[238px] z-50"></div>
-      <div className="p-4 bg-amber-400 opacity-90 w-2 rounded-full absolute -translate-x-[100px] translate-y-[238px] right-0 z-50"></div>
+      <div className="p-4 bg-yellow-300 opacity-90 w-2 rounded-full absolute  translate-x-[100px] translate-y-[238px] z-50"></div>
+      <div className="p-4 bg-yellow-300 opacity-90 w-2 rounded-full absolute -translate-x-[100px] translate-y-[238px] right-0 z-50"></div>
       <div className="p-1 w-[130px] bg-white rounded-lg absolute rotate-[30deg] translate-y-[285px] translate-x-[120px]"></div>
       <div className="p-1 w-[130px] bg-white rounded-lg absolute -rotate-[30deg] translate-y-[285px] -translate-x-[120px] right-0"></div>
-      <div className="p-4 bg-amber-400 w-2 rounded-full absolute left-1/2 -translate-x-1/2 translate-y-[305px] z-50"></div>
+      <div className="p-4 bg-yellow-300 w-2 rounded-full absolute left-1/2 -translate-x-1/2 translate-y-[305px] z-50"></div>
       <div className="p-1 w-[283px] bg-white rounded-lg absolute -rotate-[30deg] translate-y-[175px] translate-x-[110px]"></div>
       <div className="p-1 w-[283px] bg-white rounded-lg absolute rotate-[30deg] translate-y-[175px] translate-x-[110px] "></div>
       <div className="p-1 h-full opacity-25 bg-white rounded-lg absolute  left-1/2 -translate-x-1/2"></div>
-      <div className="p-4 bg-amber-400 w-2 rounded-full absolute left-1/2 -translate-x-1/2 translate-y-[160px] z-50"></div>
+      <div className="p-4 bg-yellow-300 w-2 rounded-full absolute left-1/2 -translate-x-1/2 translate-y-[160px] z-50"></div>
     </div>
   )
 }
