de24df359cd3d4e3300468432f8941a925593a7c
pedroacamargo <pedroaugennes@gmail.com>
2024-02-20 01:49:33 +0000

    Last touches navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -37,30 +37,34 @@ const Nav = (props) => {
         <Link href="/" className="nav_btn text-base">
           Home
         </Link>
-        <div className="w-[1px] h-[20px] rounded-full bg-white"></div>
+        <div className="w-[1px] h-[20px] rounded-full bg-white/50"></div>
         <Link href="/schedule" className="nav_btn text-base">
           Schedule
         </Link>
-        <div className="w-[1px] h-[20px] rounded-full bg-white"></div>
+        <div className="w-[1px] h-[20px] rounded-full bg-white/50"></div>
         <Link href="/faqs" className="nav_btn text-base">
           FAQs
         </Link>
-        <div className="w-[1px] h-[20px] rounded-full bg-white"></div>
         {user.role == "ADMIN" && (
-          <Link href="/admin" className="nav_btn text-base">
-            Backoffice
-          </Link>
+          <>
+            <div className="w-[1px] h-[20px] rounded-full bg-white/50"></div>
+            <Link href="/admin" className="nav_btn text-base">
+              Backoffice
+            </Link>
+          </>
         )}
 
         {user.name ? (
           <div className="flex w-full justify-end items-center lg:gap-3 gap-2">
-            <Link href="/profile" className="nav_btn text-base">
-              Logged as <strong className="font-bold text-lg ml-2">{user.name}</strong>
+            <Link href={`/profile/${user.id}`} className="nav_btn text-base">
+              Logged as{" "}
+              <strong className="font-bold text-lg ml-2">{user.name}</strong>
             </Link>
 
             <Button
               className="font-poppins font-normal text-base text-white bg-blue-950"
-              onClick={signOut}>
+              onClick={signOut}
+            >
               Sign Out
             </Button>
           </div>
@@ -70,49 +74,61 @@ const Nav = (props) => {
             <span className="text-white font-poppins text-sm">Loading</span>
           </div>
         ) : (
-          <Button
-            color="white"
-            variant="ghost"
-            className="font-comfortaa font-bold text-white"
-            onClick={() => router.push("/auth/signin")}
-          >
-            Sign In
-          </Button>
+          <>
+            <div className="w-[1px] h-[20px] rounded-full bg-white/50"></div>
+
+            <Button
+              color="white"
+              variant="ghost"
+              className="font-comfortaa font-bold text-white"
+              onClick={() => router.push("/auth/signin")}
+            >
+              Sign In
+            </Button>
+          </>
         )}
       </div>
 
       {/* Mobile  */}
 
       <div className="md:hidden flex">
-        <div className={`absolute w-full h-screen flex flex-col justify-center items-center backdrop-blur-sm bg-neutral-500/50 duration-1000 ${toggleDropdown ? "transform-none" : "-translate-x-full"}`}>
+        <div
+          className={`absolute w-full h-screen flex flex-col justify-center items-center backdrop-blur-sm bg-neutral-500/50 duration-1000 ${
+            toggleDropdown ? "transform-none" : "-translate-x-full"
+          }`}
+        >
           <div className="flex flex-col gap-4 w-full">
             <Link
               href="/"
               className="nav_btn text-2xl "
-              onClick={() => setToggleDropdown(false)}>
+              onClick={() => setToggleDropdown(false)}
+            >
               Home
             </Link>
 
             <Link
               href="/schedule"
               className="nav_btn text-2xl"
-              onClick={() => setToggleDropdown(false)}>
+              onClick={() => setToggleDropdown(false)}
+            >
               Schedule
             </Link>
 
             <Link
               href="/faqs"
               className="nav_btn text-2xl"
-              onClick={() => setToggleDropdown(false)}>
+              onClick={() => setToggleDropdown(false)}
+            >
               FAQs
             </Link>
 
             {user.name ? (
               <>
                 <Link
-                  href="/profile"
+                  href={`/profile/${user.id}`}
                   className="nav_btn text-2xl "
-                  onClick={() => setToggleDropdown(false)}>
+                  onClick={() => setToggleDropdown(false)}
+                >
                   Profile
                 </Link>
 
@@ -121,7 +137,8 @@ const Nav = (props) => {
                     setToggleDropdown(false);
                     signOut();
                   }}
-                  className="blue_btn">
+                  className="blue_btn"
+                >
                   Sign Out
                 </Button>
               </>
@@ -129,28 +146,36 @@ const Nav = (props) => {
               <Button
                 className="blue_btn"
                 onClick={() => {
-                  router.push("/auth/signin")
+                  router.push("/auth/signin");
                   setToggleDropdown(false);
-                }
-                }
+                }}
               >
                 Sign in
               </Button>
             )}
           </div>
         </div>
-        <div onClick={() => setToggleDropdown(!toggleDropdown)} className="flex-col flex gap-1 items-end   w-11/12  m-auto py-4 cursor-pointer">
-
-          <div className={`h-[4px] w-[24px] rounded-3xl bg-white  transition-all ${toggleDropdown && "rotate-45 translate-y-2"}`}>
-          </div>
-          <div className={`h-[4px] w-[24px] bg-white rounded-3xl duration-700  ${toggleDropdown && "-translate-y-32 -translate-x-32 rotate-180"}`}>
-          </div>
-          <div className={`h-[4px] w-[24px] bg-white rounded-3xl transition-all  ${toggleDropdown && "-rotate-45 -translate-y-2"}`}>
-          </div>
-
+        <div
+          onClick={() => setToggleDropdown(!toggleDropdown)}
+          className="flex-col flex gap-1 items-end   w-11/12  m-auto py-4 cursor-pointer"
+        >
+          <div
+            className={`h-[4px] w-[24px] rounded-3xl bg-white  transition-all ${
+              toggleDropdown && "rotate-45 translate-y-2"
+            }`}
+          ></div>
+          <div
+            className={`h-[4px] w-[24px] bg-white rounded-3xl duration-700  ${
+              toggleDropdown && "-translate-y-32 -translate-x-32 rotate-180"
+            }`}
+          ></div>
+          <div
+            className={`h-[4px] w-[24px] bg-white rounded-3xl transition-all  ${
+              toggleDropdown && "-rotate-45 -translate-y-2"
+            }`}
+          ></div>
         </div>
       </div>
-
     </nav>
   );
 };
