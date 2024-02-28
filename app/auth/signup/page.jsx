0f8adbc11fda7f4c0ff4312767ed9cf0906895d0
Edgar Araújo <edgararaj@gmail.com>
2024-02-28 00:40:46 +0000

    Signup: Add zod validation

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -10,200 +10,213 @@ import axios from "axios";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserSchema } from "/prisma/zod";
 
 export default function SignUpPage() {
-  const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(null);
   const router = useRouter();
 
-  const handleChange = (e) => {
-    // console.log(formData);
-    const value = e.target.value;
-    const name = e.target.name;
-    setFormData((prevState) => ({ ...prevState, [name]: value }));
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(UserSchema),
+  });
 
-  const handleSubmit = async (e) => {
-    try {
-      const res = await axios.post("/api/users/register", formData);
-
+  const onSubmit = (formData) => {
+    axios.post("/api/users/register", formData).then((res) => {
       if (res.status == 200) {
-        signIn("credentials", {
-          email: formData.email,
-          password: formData.password,
-          callbackUrl: "/",
-        }).then((res) => {
-          if (res.ok) {
-            router.push("/");
-            setError(false);
-          } else {
-            setError(true);
-          }
-        })
+        if (res.data.response == "error") {
+          setError(res.data.error);
+        } else {
+          signIn("credentials", {
+            email: formData.email,
+            password: formData.password,
+            callbackUrl: "/",
+          }).then((res) => {
+            if (res.ok) {
+              router.push("/");
+            }
+          });
+        }
       }
-
-    } catch (error) {
-      // TODO: Error handling
-      console.log(error);
-    }
+    });
   };
 
   return (
     <div className="h-screen bg-gradient-to-b to-20% from-custom-blue-1 to-custom-blue-3 flex justify-around items-center">
-
-      <Image width={500} height={500} src="/sci-logo.png" alt="Logo" className="lg:block hidden"></Image>
+      <Image
+        width={500}
+        height={500}
+        src="/sci-logo.png"
+        alt="Logo"
+        className="lg:block hidden"
+      ></Image>
 
       <div className="flex justify-center items-center flex-col ">
-        <h1 className="font-bold text-4xl text-white cursor-default ">Register</h1>
+        <h1 className="font-bold text-4xl text-white cursor-default ">
+          Register
+        </h1>
         <h2 className="text-md mt-2 -mb-2 text-white cursor-default">
           Let&apos;s discover a new world together!
         </h2>
-        <Input
-          type="text"
-          placeholder="Username"
-          labelPlacement="outside"
-          className="w-[300px] mt-5 text-white"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          classNames={{
-            input: [
-              "border-none",
-              "border-b-2",
-              "rounded-none",
-              "rounded-b-lg",
-              "light:text-black",
-              "",
-              "font-normal",
-              "text-md",
-              "placeholder:text-white/60",
-            ],
-            innerWrapper: ["bg-transparent"],
-            inputWrapper: [
-              "shadow-md",
-              "bg-custom-blue-3",
-              "backdrop-blur-xl",
-              "backdrop-saturate-200",
-              "hover:bg-default-0/70",
-              "dark:hover:bg-default/70",
-              "group-data-[focused=true]:bg-default-200/50",
-              "dark:group-data-[focused=true]:bg-default/60",
-              "!cursor-text",
-            ],
-          }}
-          color="primary"
-          variant="bordered"
-          size="lg"
-          startContent={
-            <CgProfile className="text-2xl text-white pointer-events-none flex-shrink-0" />
-          }
-        />
-        <Input
-          type="password"
-          placeholder="********"
-          labelPlacement="outside"
-          className="w-[300px] mt-2 text-white"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          classNames={{
-            input: [
-              "border-none",
-              "border-b-2",
-              "border-custom-blue-2",
-              "rounded-none",
-              "rounded-b-lg",
-              "light:text-black",
-              "dark:text-white",
-              "",
-              "font-normal",
-              "text-xl",
-              "placeholder:text-white/60",
-            ],
-            innerWrapper: ["bg-transparent"],
-            inputWrapper: [
-              "shadow-md",
-              "bg-custom-blue-3",
-              "backdrop-blur-xl",
-              "backdrop-saturate-200",
-              "hover:bg-default-0/70",
-              "dark:hover:bg-default/70",
-              "group-data-[focused=true]:bg-default-200/50",
-              "dark:group-data-[focused=true]:bg-default/60",
-              "!cursor-text",
-            ],
-          }}
-          color="primary"
-          variant="bordered"
-          size="lg"
-          startContent={
-            <RiLockPasswordLine className="text-2xl text-white pointer-events-none flex-shrink-0" />
-          }
-        />
-
-        <Input
-          type="email"
-          placeholder="you@example.com"
-          labelPlacement="outside"
-          className="w-[300px] mt-2 text-white"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          classNames={{
-            input: [
-              "border-none",
-              "border-b-2",
-              "border-custom-blue-2",
-              "rounded-none",
-              "rounded-b-lg",
-              "light:text-black",
-              "dark:text-white",
-              "",
-              "font-normal",
-              "text-md",
-              "placeholder:text-white/60",
-            ],
-            innerWrapper: ["bg-transparent"],
-            inputWrapper: [
-              "shadow-md",
-              "bg-custom-blue-3",
-              "backdrop-blur-xl",
-              "backdrop-saturate-200",
-              "hover:bg-default-0/70",
-              "dark:hover:bg-default/70",
-              "group-data-[focused=true]:bg-default-200/50",
-              "dark:group-data-[focused=true]:bg-default/60",
-              "!cursor-text",
-            ],
-          }}
-          size="lg"
-          color="primary"
-          variant="bordered"
-          startContent={
-            <CiMail className="text-2xl text-white pointer-events-none flex-shrink-0" />
-          }
-        />
-
-        <Link
-          href="/auth/signin"
-          className=" text-sm mt-3 text-white cursor-pointer font-normal hover:text-white/80"
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col items-center justify-center gap-1 mt-1"
+          noValidate
         >
-          Already have an account? &nbsp;
-          <span className="font-bold">
-            Sign In!
-          </span>
-        </Link>
-        {error && <div className="bg-red-500 p-2 text-white mt-3 rounded">An error happened</div>}
-        <Button
-          className="mt-3 w-[300px] lg:font-normal text-lg text-black bg-white font-bold "
-          size="large"
-          auto
-          color="primary"
-          variant="shadow"
-          onPress={handleSubmit}
-        >
-          Submit
-        </Button>
+          <Input
+            type="text"
+            placeholder="Username"
+            labelPlacement="outside"
+            className="w-[300px] mt-5 text-white"
+            classNames={{
+              input: [
+                "border-none",
+                "border-b-2",
+                "rounded-none",
+                "rounded-b-lg",
+                "light:text-black",
+                "",
+                "font-normal",
+                "text-md",
+                "placeholder:text-white/60",
+              ],
+              innerWrapper: ["bg-transparent"],
+              inputWrapper: [
+                "shadow-md",
+                "bg-custom-blue-3",
+                "backdrop-blur-xl",
+                "backdrop-saturate-200",
+                "hover:bg-default-0/70",
+                "dark:hover:bg-default/70",
+                "group-data-[focused=true]:bg-default-200/50",
+                "dark:group-data-[focused=true]:bg-default/60",
+                "!cursor-text",
+              ],
+            }}
+            color="primary"
+            variant="bordered"
+            size="lg"
+            startContent={
+              <CgProfile className="text-2xl text-white pointer-events-none flex-shrink-0" />
+            }
+            isInvalid={!!errors.name}
+            errorMessage={errors.name?.message}
+            {...register("name")}
+          />
+          <Input
+            type="password"
+            placeholder="********"
+            labelPlacement="outside"
+            className="w-[300px] mt-2 text-white"
+            classNames={{
+              input: [
+                "border-none",
+                "border-b-2",
+                "border-custom-blue-2",
+                "rounded-none",
+                "rounded-b-lg",
+                "light:text-black",
+                "dark:text-white",
+                "",
+                "font-normal",
+                "text-xl",
+                "placeholder:text-white/60",
+              ],
+              innerWrapper: ["bg-transparent"],
+              inputWrapper: [
+                "shadow-md",
+                "bg-custom-blue-3",
+                "backdrop-blur-xl",
+                "backdrop-saturate-200",
+                "hover:bg-default-0/70",
+                "dark:hover:bg-default/70",
+                "group-data-[focused=true]:bg-default-200/50",
+                "dark:group-data-[focused=true]:bg-default/60",
+                "!cursor-text",
+              ],
+            }}
+            color="primary"
+            variant="bordered"
+            size="lg"
+            startContent={
+              <RiLockPasswordLine className="text-2xl text-white pointer-events-none flex-shrink-0" />
+            }
+            isInvalid={!!errors.password}
+            errorMessage={errors.password?.message}
+            {...register("password")}
+          />
+
+          <Input
+            type="email"
+            placeholder="you@example.com"
+            labelPlacement="outside"
+            className="w-[300px] mt-2 text-white"
+            classNames={{
+              input: [
+                "border-none",
+                "border-b-2",
+                "border-custom-blue-2",
+                "rounded-none",
+                "rounded-b-lg",
+                "light:text-black",
+                "dark:text-white",
+                "",
+                "font-normal",
+                "text-md",
+                "placeholder:text-white/60",
+              ],
+              innerWrapper: ["bg-transparent"],
+              inputWrapper: [
+                "shadow-md",
+                "bg-custom-blue-3",
+                "backdrop-blur-xl",
+                "backdrop-saturate-200",
+                "hover:bg-default-0/70",
+                "dark:hover:bg-default/70",
+                "group-data-[focused=true]:bg-default-200/50",
+                "dark:group-data-[focused=true]:bg-default/60",
+                "!cursor-text",
+              ],
+            }}
+            size="lg"
+            color="primary"
+            variant="bordered"
+            startContent={
+              <CiMail className="text-2xl text-white pointer-events-none flex-shrink-0" />
+            }
+            isInvalid={!!errors.email}
+            errorMessage={errors.email?.message}
+            {...register("email")}
+          />
+
+          <Link
+            href="/auth/signin"
+            className=" text-sm mt-3 text-white cursor-pointer font-normal hover:text-white/80"
+          >
+            Already have an account? &nbsp;
+            <span className="font-bold">Sign In!</span>
+          </Link>
+          {error && (
+            <div className="bg-red-500 p-2 text-white mt-3 rounded">
+              {error}
+            </div>
+          )}
+          <Button
+            type="submit"
+            className="mt-3 w-[300px] lg:font-normal text-lg text-black bg-white font-bold "
+            size="large"
+            auto
+            color="primary"
+            variant="shadow"
+          >
+            Submit
+          </Button>
+        </form>
       </div>
     </div>
   );
