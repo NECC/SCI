32777d72a5be0c5db46d11d129ce93155d1db2f9
Edgar Araújo <edgararaj@gmail.com>
2024-03-05 07:54:14 +0000

    SEO: Page titles, favicon, and opengraph

---
+++ app/faqs/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const metadata = {
+  title: 'FAQs',
+};
+
+function Layout({children}) {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default Layout
