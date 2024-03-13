986becb32159f41b457af077d3d5e2095202620e
Edgar Araújo <edgararaj@gmail.com>
2024-03-13 15:05:36 +0000

    API: Create csv file for ranking purposes

--- app/api/ranking/route.js
+++ app/api/ranking/route.js
@@ -7,6 +7,72 @@ const prisma = new PrismaClient();
 
 
 /**
+ * Get csv file of user points
+ * @method POST
+ * @returns [{ user, points }]
+**/
+export async function GET() {
+  // const data = await request.json();
+  const session = await getServerSession(authOptions);
+
+
+  if (session?.user.role != "ADMIN") {
+    prisma.$disconnect();
+    return new NextResponse(
+      JSON.stringify({
+        response: "error",
+        error: "You don't have permission to get the ranking!",
+      })
+    );
+  }
+
+  try {
+    // get csv file of user id, email, name, and points
+
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            email: true,
+            name: true,
+            points: true,
+            role: true
+        }
+    })
+
+    // create csv file where each line of id email name appears points times
+
+    let csv = "id,email,name\n";
+
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].role == "USER") {
+        for (let j = 0; j < users[i].points; j++) {
+          csv += `${users[i].id},${users[i].email},${users[i].name}\n`;
+        }
+      }
+    }
+
+    prisma.$disconnect();
+
+    // return csv file
+    return new NextResponse(
+      csv,
+      {
+        headers: {
+          "Content-Type": "text/csv",
+          "Content-Disposition": `attachment; filename="users.csv"`,
+        },
+      }
+    );
+
+  } catch (error) {
+    prisma.$disconnect();
+    return new NextResponse(
+      JSON.stringify({ response: "error", error: error })
+    );
+  }
+}
+
+/**
  * Increments the points of an User by 1
  * @method POST
  * @returns [{ user, points }]
@@ -41,7 +107,7 @@ export async function POST(request) {
     return new NextResponse(
       JSON.stringify({ response: "success", increment: { user: increment.id, points: increment.points }})
     );
-    
+
   } catch (error) {
     prisma.$disconnect();
     return new NextResponse(
