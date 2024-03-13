14895c3f6b0edbf251f974f0661777b855a26630
pedroacamargo <pedroaugennes@gmail.com>
2024-03-13 17:28:45 +0000

    faqs

--- components/Accordion.jsx
+++ components/Accordion.jsx
@@ -7,12 +7,14 @@ export default function Accordion() {
   const participate = "Of course! SCI is open to everyone, regardless if you’re a student or not. Everyone is welcome to participate in our workshops and our talks.";
   const register = "You can participate by registering in this year’s edition through the link below.";
   const contact = " You can email us at sci.uminho@gmail.com or send us a message through our social media accounts.";
+  const points = "In SCI every activity you are part of gives you 1 point! At the end of every activity, reach someone from the Staff so they can scan the QR Code on your profile and give you the point."
 
   const [active, setActive] = useState(false);
   const [active2, setActive2] = useState(false);
   const [active3, setActive3] = useState(false);
   const [active4, setActive4] = useState(false);
   const [active5, setActive5] = useState(false);
+  const [active6, setActive6] = useState(false);
 
 
   // TODO: Refactor this component to use a map function to render the accordions
@@ -61,11 +63,21 @@ export default function Accordion() {
 
       <div className='bg-gradient-to-r from-custom-blue-3 to-custom-blue-1 w-full rounded-lg border-1 border-solid'>
         <div className='flex items-center  justify-between p-3 cursor-pointer font-poppins text-white font-normal sm:text-lg text-base' onClick={() => setActive5(!active5)}>
-          <p className="w-[80%]">How can I contact SCI’s team?</p>
+          <p className="w-[80%]">How can I contact SCI&apos;s team?</p>
           <IoArrowDownCircleOutline size={30} color="white" className={`ease-in-out duration-300 ${active5 ? 'rotate-180' : 'rotate-0'}`} />
         </div>
         <div className={`bg-white rounded-b-md overflow-hidden ease-in-out duration-300 ${active5 ? 'max-h-[500px]' : 'max-h-0'}`}>
           <p className="p-3">{contact}</p>
+        </div>
+      </div>
+
+      <div className='bg-gradient-to-r from-custom-blue-3 to-custom-blue-1 w-full rounded-lg border-1 border-solid'>
+        <div className='flex items-center  justify-between p-3 cursor-pointer font-poppins text-white font-normal sm:text-lg text-base' onClick={() => setActive6(!active6)}>
+          <p className="w-[80%]">How does the system points work?</p>
+          <IoArrowDownCircleOutline size={30} color="white" className={`ease-in-out duration-300 ${active6 ? 'rotate-180' : 'rotate-0'}`} />
+        </div>
+        <div className={`bg-white rounded-b-md overflow-hidden ease-in-out duration-300 ${active6 ? 'max-h-[500px]' : 'max-h-0'}`}>
+          <p className="p-3">{points}</p>
         </div>
       </div>
     </div>
