a8dca3babffd10c1e642f205b0765d31bb2ea4ff
luissilva <biokiller134@gmail.com>
2025-04-22 11:21:01 +0100

    Test qrcode

--- app/staff/qrcode/page.tsx
+++ app/staff/qrcode/page.tsx
@@ -1,5 +1,11 @@
 "use client";
-import { Scanner } from "@yudiel/react-qr-scanner";
+import { 
+    Scanner,
+    useDevices,
+  outline,
+  boundingBox,
+  centerText,
+ } from "@yudiel/react-qr-scanner";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -10,13 +16,41 @@ import { EnrollmentAttendQRCodePostResponse } from "@app/api/enrollments/attend/
 import { UserUpdateResponse } from "@app/api/users/[id]/route";
 import { ActivityPointsResponse } from "@app/api/activities/[id]/points/route";
 
+const styles = {
+    container: {
+      width: 400,
+      margin: "auto",
+    },
+    controls: {
+      marginBottom: 8,
+    },
+  };
+
 export default function QRCodeReader() {
 const [user, setUser] = useState<{
     user: { email: string; role: string } | null;
     loaded: boolean;
 }>({ user: null, loaded: false });
-const [pause, setPause] = useState(false);
+
 const [success, setSuccess] = useState(false);
+const [deviceId, setDeviceId] = useState<string | undefined>(undefined);
+  const [tracker, setTracker] = useState<string | undefined>("centerText");
+  const [pause, setPause] = useState(false);
+
+  const devices = useDevices();
+
+  function getTracker() {
+    switch (tracker) {
+      case "outline":
+        return outline;
+      case "boundingBox":
+        return boundingBox;
+      case "centerText":
+        return centerText;
+      default:
+        return undefined;
+    }
+  }
 
 const router = useRouter();
 
@@ -79,48 +113,67 @@ if (user.user?.role == "USER" && user.loaded) router.push("/");
 
 return (
     <div className="flex justify-center flex-col items-center w-full h-screen bg-gradient-to-l from-custom-blue-3 to-custom-blue-1">
-    <Scanner
-    formats={[
-        "qr_code",
-        "micro_qr_code",
-        "rm_qr_code",
-        "maxi_code",
-        "pdf417",
-        "aztec",
-        "data_matrix",
-        "matrix_codes",
-        "dx_film_edge",
-        "databar",
-        "databar_expanded",
-        "codabar",
-        "code_39",
-        "code_93",
-        "code_128",
-        "ean_8",
-        "ean_13",
-        "itf",
-        "linear_codes",
-        "upc_a",
-        "upc_e",
-    ]}
-    onScan={(detectedCodes) => {
-        handleScan(detectedCodes[0].rawValue);
-    }}
-    onError={(error) => {
-        console.log(`onError: ${error}'`);
-    }}
-    styles={{ container: { height: "400px", width: "350px" } }}
-    components={{
-        audio: true,
-        onOff: true,
-        torch: true,
-        zoom: true,
-        finder: true,
-    }}
-    allowMultiple={true}
-    scanDelay={2000}
-    paused={pause}
-    />
+        <div style={styles.controls}>
+        <select onChange={(e) => setDeviceId(e.target.value)}>
+          <option value={undefined}>Select a device</option>
+          {devices.map((device, index) => (
+            <option key={index} value={device.deviceId}>
+              {device.label}
+            </option>
+          ))}
+        </select>
+        <select
+          style={{ marginLeft: 5 }}
+          onChange={(e) => setTracker(e.target.value)}
+        >
+          <option value="centerText">Center Text</option>
+          <option value="outline">Outline</option>
+          <option value="boundingBox">Bounding Box</option>
+          <option value={undefined}>No Tracker</option>
+        </select>
+      </div>
+        <Scanner
+        formats={[
+            "qr_code",
+            "micro_qr_code",
+            "rm_qr_code",
+            "maxi_code",
+            "pdf417",
+            "aztec",
+            "data_matrix",
+            "matrix_codes",
+            "dx_film_edge",
+            "databar",
+            "databar_expanded",
+            "codabar",
+            "code_39",
+            "code_93",
+            "code_128",
+            "ean_8",
+            "ean_13",
+            "itf",
+            "linear_codes",
+            "upc_a",
+            "upc_e",
+        ]}
+        onScan={(detectedCodes) => {
+            handleScan(detectedCodes[0].rawValue);
+        }}
+        onError={(error) => {
+            console.log(`onError: ${error}'`);
+        }}
+        styles={{ container: { height: "400px", width: "350px" } }}
+        components={{
+            audio: true,
+            onOff: true,
+            torch: true,
+            zoom: true,
+            finder: true,
+        }}
+        allowMultiple={true}
+        scanDelay={2000}
+        paused={pause}
+        />
     {success && (
         <div className="bg-green-500 p-4 rounded-md mt-4">
         <h1 className="text-white">Success</h1>
